ccc21143a4ce1bfb09e26bea428b2b048cff6184
CodeWithAli <151702013+alisulman@users.noreply.github.com>
2024-09-17 19:48:48 +0500

    updates

--- src/UI/fonts.ts
+++ src/UI/fonts.ts
@@ -1,8 +1,9 @@
 import { Poppins, Protest_Guerrilla } from "next/font/google";
+import localFont from "next/font/local";
 
-export const PG = Protest_Guerrilla({
-    subsets: ["latin"],
-    weight: "400",
+export const PG = localFont({
+    src: "../app/fonts/ProtestGuerrilla-Regular.ttf",
+    weight: "100 900",
 });
 
 export const poppins = Poppins({
@@ -10,3 +11,12 @@ export const poppins = Poppins({
     weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
     preload: true
 })
+
+export const geistSans = localFont({
+    src: "../app/fonts/GeistVF.woff",
+    weight: "100 900",
+});
+export const geistMono = localFont({
+    src: "../app/fonts/GeistMonoVF.woff",
+    weight: "100 900"
+});
